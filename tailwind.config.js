e3800a30d15f4e2d70405184b135c652facd5088
Robert Turner <robturneris@gmail.com>
2022-03-29 22:07:07 -0400

    Added Inter font, tailwind UI, and purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+const tailwindForms = require('@tailwindcss/forms')
+const tailwindAspectRatio = require('@tailwindcss/aspect-ratio')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  plugins: [tailwindForms, tailwindAspectRatio],
 }
