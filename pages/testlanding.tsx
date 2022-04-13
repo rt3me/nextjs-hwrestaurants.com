5e203d91bc67314ec7f598cb92e0fff715779fcc
Robert Turner <robturneris@gmail.com>
2022-04-12 23:35:18 -0400

    Added information section

--- pages/testlanding.tsx
+++ pages/testlanding.tsx
@@ -195,6 +195,128 @@ const TestLanding: NextPage = () => (
       </div>
     </header>
     <main>
+      {/* Contact section */}
+      <div className="bg-black">
+        <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
+          <div className="divide-y-2 divide-gray-500">
+            <div className="lg:grid lg:grid-cols-3 lg:gap-8">
+              <h2 className="text-2xl font-extrabold text-white sm:text-3xl">
+                Details
+              </h2>
+              <div className="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:col-span-2 lg:mt-0">
+                <div>
+                  <h3 className="text-lg font-medium leading-6 text-gray-300">
+                    Get in touch
+                  </h3>
+                  <dl className="mt-2 text-base text-gray-500">
+                    <div>
+                      <dt className="sr-only">Address</dt>
+                      <dd>3223 Lemmon Ave #103</dd>
+                      <dd>Dallas, TX 75204</dd>
+                    </div>
+                    <div className="mt-1">
+                      <dt className="sr-only">Phone number</dt>
+                      <dd>phone: 214-954-9558</dd>
+                      <dd>fax: 214-954-0988</dd>
+                    </div>
+                  </dl>
+                </div>
+                <div>
+                  <h3 className="text-lg font-medium leading-6 text-gray-300">
+                    Hours of operation
+                  </h3>
+                  <dl className="mt-2 text-base text-gray-500">
+                    <div>
+                      <dt className="sr-only">Hours of operation</dt>
+                      <dd>Mon-Thur: 11am - 2pm; 4pm - 9pm</dd>
+                      <dd>Fri: 11am - 2pm; 4pm - 10pm</dd>
+                      <dd>Sat: 11:30am - 10pm</dd>
+                      <dd>Sun: 11:30am - 9pm</dd>
+                    </div>
+                    <div className="mt-1">
+                      <dt className="sr-only">Happy hour</dt>
+                      <dd>Happy hour lounge and patio only:</dd>
+                      <dd>Mon-Fri: 4pm - 7pm</dd>
+                    </div>
+                  </dl>
+                </div>
+                <div>
+                  <h3 className="text-lg font-medium leading-6 text-gray-300">
+                    Delivery, takeout
+                  </h3>
+                  <dl className="mt-2 text-base text-gray-500">
+                    <div>
+                      <dt className="sr-only">Delivery and takeout</dt>
+                      <dd>
+                        Please call us for take out and delivery orders. We
+                        offer curbside pick up and deliver within a 2.5 mile
+                        radius.
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
+                <div>
+                  <h3 className="text-lg font-medium leading-6 text-gray-300">
+                    Reservations
+                  </h3>
+                  <dl className="mt-2 text-base text-gray-500">
+                    <div>
+                      <dt className="sr-only" id="#reservations">
+                        Reservations
+                      </dt>
+                      <dd>Please call the restaurant to make a reservation.</dd>
+                    </div>
+                  </dl>
+                </div>
+              </div>
+            </div>
+            <div className="mt-16 pt-16 lg:grid lg:grid-cols-3 lg:gap-8">
+              <h2 className="text-2xl font-extrabold text-white sm:text-3xl">
+                Locations
+              </h2>
+              <div className="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:col-span-2 lg:mt-0">
+                <div>
+                  <h3 className="text-lg font-medium leading-6 text-gray-300">
+                    Los Angeles
+                  </h3>
+                  <div className="mt-2 text-base text-gray-500">
+                    <p>4556 Brendan Ferry</p>
+                    <p className="mt-1">Los Angeles, CA 90210</p>
+                  </div>
+                </div>
+                <div>
+                  <h3 className="text-lg font-medium leading-6 text-gray-300">
+                    New York
+                  </h3>
+                  <div className="mt-2 text-base text-gray-500">
+                    <p>886 Walter Streets</p>
+                    <p className="mt-1">New York, NY 12345</p>
+                  </div>
+                </div>
+                <div>
+                  <h3 className="text-lg font-medium leading-6 text-gray-300">
+                    Toronto
+                  </h3>
+                  <div className="mt-2 text-base text-gray-500">
+                    <p>7363 Cynthia Pass</p>
+                    <p className="mt-1">Toronto, ON N3Y 4H8</p>
+                  </div>
+                </div>
+                <div>
+                  <h3 className="text-lg font-medium leading-6 text-gray-300">
+                    Chicago
+                  </h3>
+                  <div className="mt-2 text-base text-gray-500">
+                    <p>726 Mavis Island</p>
+                    <p className="mt-1">Chicago, IL 60601</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* About section */}
       <div className="relative bg-white">
         <div className="lg:absolute lg:inset-0">
           <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
