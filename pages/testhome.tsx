f976b1cea95a39aec8bbf97def735e4fd8441165
Robert Turner <robturneris@gmail.com>
2022-04-18 19:31:31 -0400

    Updated CTA section

--- pages/testhome.tsx
+++ pages/testhome.tsx
@@ -465,24 +465,23 @@ const TestHome: NextPage = () => (
       <div className="bg-white">
         <div className="mx-auto max-w-4xl py-16 px-4 sm:px-6 sm:py-24 lg:flex lg:max-w-7xl lg:items-center lg:justify-between lg:px-8">
           <h2 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            <span className="block">Ready to get started?</span>
-            <span className="-mb-1 block bg-gradient-to-r from-purple-600 to-indigo-600 bg-clip-text pb-1 text-transparent">
-              Get in touch or create an account.
+            <span className="block">Why not treat yourself?</span>
+            <span className="-mb-1 block pb-1 text-red-700">
+              Order to go from Mandarin, by Howard Wang&lsquo;s.
             </span>
           </h2>
           <div className="mt-6 space-y-4 sm:flex sm:space-y-0 sm:space-x-5">
             <a
-              href="#"
-              className="flex items-center justify-center rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-3 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700"
+              href="tel:4697944343"
+              className="flex items-center justify-center rounded-md border border-transparent bg-red-700 bg-origin-border px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-red-600"
             >
-              Learn more
+              Call Now
             </a>
-            <a
-              href="#"
-              className="flex items-center justify-center rounded-md border border-transparent bg-indigo-50 px-4 py-3 text-base font-medium text-indigo-800 shadow-sm hover:bg-indigo-100"
-            >
-              Get started
-            </a>
+            <Link href="/testlanding">
+              <a className="flex items-center justify-center rounded-md border border-transparent bg-black px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-gray-900">
+                Learn More
+              </a>
+            </Link>
           </div>
         </div>
       </div>
