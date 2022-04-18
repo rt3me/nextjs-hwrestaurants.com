b1f6ad3011df43a033b73118b0655393c15f5ca2
Robert Turner <robturneris@gmail.com>
2022-04-18 14:04:54 -0400

    Fixed alternating images

--- pages/testhome.tsx
+++ pages/testhome.tsx
@@ -357,12 +357,13 @@ const TestHome: NextPage = () => (
               </div>
             </div>
             <div className="mt-12 sm:mt-16 lg:mt-0">
-              <div className="-mr-48 pl-4 sm:pl-6 md:-mr-16 lg:relative lg:m-0 lg:h-full lg:px-0">
+              <div className="relative -mr-48 pl-4">
                 <Image
-                  className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                  className="absolute left-0 w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
                   src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
                   alt="Inbox user interface"
-                  layout="fill"
+                  width="952"
+                  height="571"
                 />
               </div>
             </div>
@@ -370,7 +371,7 @@ const TestHome: NextPage = () => (
         </div>
         <div className="mt-24">
           <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
-            <div className="mx-auto max-w-xl px-4 sm:px-6 lg:col-start-2 lg:mx-0 lg:max-w-none lg:py-32 lg:px-0">
+            <div className="mx-auto max-w-xl px-4 sm:px-6 lg:col-start-2 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0">
               <div>
                 <div>
                   <span className="flex h-12 w-12 items-center justify-center rounded-md bg-gradient-to-r from-purple-600 to-indigo-600">
@@ -403,12 +404,13 @@ const TestHome: NextPage = () => (
               </div>
             </div>
             <div className="mt-12 sm:mt-16 lg:col-start-1 lg:mt-0">
-              <div className="-ml-48 pr-4 sm:pr-6 md:-ml-16 lg:relative lg:m-0 lg:h-full lg:px-0">
+              <div className="relative -ml-48 pr-4">
                 <Image
-                  className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                  className="absolute right-0 w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
                   src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
                   alt="Customer profile user interface"
-                  layout="fill"
+                  width="952"
+                  height="571"
                 />
               </div>
             </div>
