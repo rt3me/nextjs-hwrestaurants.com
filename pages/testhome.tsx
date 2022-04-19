1b7a73a5924b85de61e68c9d8ff72eca842f6368
Robert Turner <robturneris@gmail.com>
2022-04-18 21:40:02 -0400

    Updated hero section

--- pages/testhome.tsx
+++ pages/testhome.tsx
@@ -13,6 +13,7 @@ import {
 } from '@heroicons/react/outline'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import Footer from '../components/Footer'
+import chefFlamingWokDump from '../public/images/chef-flaming-wok-dump.jpg'
 
 const locations = [
   {
@@ -279,35 +280,37 @@ const TestHome: NextPage = () => (
             <div className="absolute inset-0">
               <Image
                 className="h-full w-full object-cover"
-                src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
-                alt="People working on laptops"
+                src={chefFlamingWokDump}
+                alt="Chef tilting flaming wok"
                 layout="fill"
               />
-              <div className="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply" />
+              <div className="absolute inset-0 bg-gradient-to-r from-red-700 to-red-600 mix-blend-multiply" />
             </div>
             <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
               <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                <span className="block text-white">Take control of your</span>
-                <span className="block text-indigo-200">customer support</span>
+                <span className="block text-white">Visit us today at</span>
+                <span className="block text-white">Howard Wang&lsquo;s</span>
               </h1>
-              <p className="mx-auto mt-6 max-w-lg text-center text-xl text-indigo-200 sm:max-w-3xl">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua.
+              <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
+                A uniquely luxurious yet welcoming group of restaurants in Texas
+                where you will find a dining experience your whole family will
+                not forget.
               </p>
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                   <a
-                    href="#"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-indigo-700 shadow-sm hover:bg-indigo-50 sm:px-8"
+                    href="#locations"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-black shadow-sm hover:bg-gray-100 sm:px-8"
                   >
-                    Get started
+                    Locations
                   </a>
                   <a
-                    href="#"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-indigo-500 bg-opacity-60 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-70 sm:px-8"
+                    href="https://order.chownow.com/order/3661/locations"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-gray-500 bg-opacity-70 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-80 sm:px-8"
+                    target="_blank"
+                    rel="noreferrer"
                   >
-                    Live demo
+                    Order Online
                   </a>
                 </div>
               </div>
@@ -419,7 +422,7 @@ const TestHome: NextPage = () => (
       </div>
 
       {/* Gradient Feature Section */}
-      <div className="bg-red-700">
+      <div id="locations" className="bg-red-700">
         <div className="mx-auto max-w-4xl px-4 py-16 sm:px-6 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:px-8 lg:pt-24">
           <h2 className="text-3xl font-extrabold tracking-tight text-white">
             Locations
