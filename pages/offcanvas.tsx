14a411b81f338df86cb1e5471bdeeb1e88c1c9d7
Robert Turner <robturneris@gmail.com>
2022-04-29 17:23:37 -0500

    Removed page heading

--- pages/offcanvas.tsx
+++ pages/offcanvas.tsx
@@ -298,11 +298,6 @@ const TestHome: NextPage = () => {
           </div>
           <main className="flex-1">
             <div className="py-6">
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <h1 className="text-2xl font-semibold text-gray-900">
-                  Dashboard
-                </h1>
-              </div>
               <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                 {/* Replace with your content */}
                 <div className="py-4">
