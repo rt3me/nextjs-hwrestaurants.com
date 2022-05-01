8d7cadbd02173bf8ee89dd4cf03831f64bf4b0e6
Robert Turner <robturneris@gmail.com>
2022-05-01 14:08:27 -0500

    Removed content area left and right padding

--- pages/offcanvas.tsx
+++ pages/offcanvas.tsx
@@ -298,7 +298,7 @@ const TestHome: NextPage = () => {
           </div>
           <main className="flex-1">
             <div className="py-6">
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+              <div className="mx-auto max-w-7xl">
                 {/* Replace with your content */}
                 <div className="py-4">
                   {/* <div className="border-4 border-gray-200 border-dashed rounded-lg h-96" /> */}
