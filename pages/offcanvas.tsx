108c5272a8fec1966a9bed2ce0733942ed5c3574
Robert Turner <robturneris@gmail.com>
2022-05-09 21:45:19 -0500

    Adjusted desktop menu background color

--- pages/offcanvas.tsx
+++ pages/offcanvas.tsx
@@ -210,7 +210,7 @@ const TestHome: NextPage = () => {
         {/* Static sidebar for desktop */}
         <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex min-h-0 flex-1 flex-col bg-gray-800">
+          <div className="flex min-h-0 flex-1 flex-col bg-black">
             <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
               <div className="flex flex-shrink-0 items-center px-4">
                 <Image
