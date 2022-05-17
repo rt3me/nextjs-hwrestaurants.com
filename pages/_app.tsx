6504c285183e85afa3c517089626b0c55aaf0470
Robert Turner <robturneris@gmail.com>
2022-05-16 23:43:56 -0500

    Updated custom app to use layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(
     <>
       <Head>
         <meta charSet="UTF-8" />
@@ -18,5 +30,3 @@ function MyApp({ Component, pageProps }: AppProps) {
     </>
   )
 }
-
-export default MyApp
